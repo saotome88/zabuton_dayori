f99581e9a21cfd7c35ef52c35750ffd18c471e85
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-12 12:23:57 +0900

    いいね機能:reaction_likesテーブルの作成/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_142417) do
+ActiveRecord::Schema.define(version: 2021_07_12_030807) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2021_07_10_142417) do
     t.index ["user_id"], name: "index_memos_on_user_id"
   end
 
+  create_table "reaction_likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_reaction_likes_on_event_id"
+    t.index ["user_id"], name: "index_reaction_likes_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,4 +55,6 @@ ActiveRecord::Schema.define(version: 2021_07_10_142417) do
 
   add_foreign_key "memos", "events"
   add_foreign_key "memos", "users"
+  add_foreign_key "reaction_likes", "events"
+  add_foreign_key "reaction_likes", "users"
 end
---
+++ spec/factories/reaction_likes.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :reaction_like do
+    user { nil }
+    event { nil }
+  end
+end
