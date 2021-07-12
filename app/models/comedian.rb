6401ad1df715425ae7a9d5e8d36036d56ebe5a46
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-12 21:46:19 +0900

    トップページ実装:comedian.rbにデータを記述

---
+++ app/models/comedian.rb
@@ -0,0 +1,11 @@
+class Comedian < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '' },
+    { id: 2, name '春風亭　あああ' },
+    { id: 3, name '柳家　いいい' },
+    { id: 4, name '橘　ううう' },
+    { id: 5, name '桂　えええ' },
+    { id: 6, name '三遊亭　おおお' },
+    { id: 7, name '神田　かかか' }
+]
+end
