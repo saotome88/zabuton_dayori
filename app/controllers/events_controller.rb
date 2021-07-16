45693e7973e44b207d02872ce03ea36e079c4e0e
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-16 23:48:07 +0900

    トップページ実装: コントローラー/スペルミス等の修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,8 +4,7 @@ class EventsController < ApplicationController
   end
 
   private
-
   def event_parameter
-    params.require(:event).permit(:theatername, :startday, :daynight, :comedian_id)
+    params.require(:event).permit(:theatername, :startday, :daynight, :event_comedian_id)
   end
 end
