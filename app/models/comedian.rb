7afa8ffdec2be5b87c00cd76f94a871012e40fbd
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-17 22:18:51 +0900

    トップページ実装:Comedianクラスのid番号修正

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -3,18 +3,13 @@ class Comedian < ActiveHash::Base
   has_many :event_comedians
   has_many :events, through: :event_comedians
 
-  # mapメソッドにより各レコードごとのeventを配列に格納している
-  def events
-    event_comedians.map(&:event)
-  end
-
   self.data = [
-    { id: 1, name: '' },
-    { id: 2, name: '春風亭　あああ' },
-    { id: 3, name: '柳家　いいい' },
-    { id: 4, name: '橘　ううう' },
-    { id: 5, name: '桂　えええ' },
-    { id: 6, name: '三遊亭　おおお' },
-    { id: 7, name: '神田　かかか' }
+    { id: 0, name: '' },
+    { id: 1, name: '春風亭　あああ' },
+    { id: 2, name: '柳家　いいい' },
+    { id: 3, name: '橘　ううう' },
+    { id: 4, name: '桂　えええ' },
+    { id: 5, name: '三遊亭　おおお' },
+    { id: 6, name: '神田　かかか' }
 ]
 end
