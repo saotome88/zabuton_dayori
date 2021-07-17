6e843a01fd2e7345fd5c8150c2f785be78f6e7b6
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-17 22:58:36 +0900

    トップページ実装:eventモデルのActiveHashアソシエーション記述

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :reaction_likes
   has_many :users, through: :reaction_likes
   has_many :event_comedians
