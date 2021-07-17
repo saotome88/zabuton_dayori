db3c00feae4c1b4da88f217d5810bee7d8990d2a
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-17 22:59:09 +0900

    トップページ実装:indexビューにアクティブハッシュのデータを表示/仮

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,12 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all
+    @event = Event.find(1)
+    @event_comedian = EventComedian.find(1)
   end
 
   private
   def event_parameter
-    params.require(:event).permit(:theatername, :startday, :daynight, :event_comedian_id)
+    params.require(:event).permit(:theater_id, :startday, :daynight_id)
   end
 end
