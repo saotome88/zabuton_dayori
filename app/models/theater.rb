497f68c0f57b374df7b19dee6c9b34b667ee1f0b
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-17 22:57:50 +0900

    トップページ実装: Theaterクラスの記述修正

--- app/models/theater.rb
+++ app/models/theater.rb
@@ -5,8 +5,8 @@ class Theater < ActiveHash::Base
   self.data = [
     { id: 0, name: '' },
     { id: 1, name: '新宿末廣亭' },
-    { id: 2, name: '池袋演芸場' }
-    { id: 3, name: '浅草演芸ホール' }
+    { id: 2, name: '池袋演芸場' },
+    { id: 3, name: '浅草演芸ホール' },
     { id: 4, name: '上野鈴本' }
-]
+  ]
 end
