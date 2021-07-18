9523d51d6b5501692eec1694eebe9f27e537bb27
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-18 19:20:33 +0900

    管理者投稿:admin_eventsコントローラーにアクション設定

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,2 +1,33 @@
 class Admin::EventsController < ApplicationController
+  #管理者ユーザー以外でアクションをしようとするとトップページに遷移する
+  before_action :if_not_admin
+
+  #管理者だけにさせたいアクションはここに記述する！
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.valid?
+      @event.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+  end
+
+  private
+  def if_not_admin
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def event_params
+    params.require(:event).permit(::theater_id, :startday, :daynight_id).merge(user_id: current_user.id)
+  end
 end
