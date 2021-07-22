6692f7bdcd6c623dfcbfcb8a5c82f8135fef29e5
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-22 22:30:33 +0900

    管理者投稿:date_select用のinclude Assignment追記/saveメソッド修正

--- app/models/event_comedian_form.rb
+++ app/models/event_comedian_form.rb
@@ -1,19 +1,19 @@
 class EventComedianForm
   include ActiveModel::Model
+  include ActiveRecord::AttributeAssignment
   extend ActiveHash::Associations::ActiveRecordExtensions
   attr_accessor :theater_id, :daynight_id, :startday, :comedian_id
 
   with_options presence: true do
     validates :theater_id
     validates :daynight_id
-#    validates :startday
+    validates :startday
     validates :comedian_id
   end
 
   def save
     event = Event.create(theater_id: theater_id, daynight_id: daynight_id, startday: startday)
-    comedian = Comedian.find(name: name)
-    EventComedianForm.create(event_id: event.id, comedian_id: comedian.id) 
+    EventComedian.create(event_id: event.id, comedian_id: comedian_id) 
   end
 
 end
