575346a96ffee6afff31628992b4d4e41f74aec5
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-23 22:30:37 +0900

    カレンダー:コントローラー修正/Event.all

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,5 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all
-    #本番環境の表示のためコメントアウト
-    #@event = Event.find(1)
-    #@event_comedian = EventComedian.find(1)
-  end
-
-  private
-  def event_parameter
-    params.require(:event).permit(:theater_id, :startday, :daynight_id)
   end
 end
