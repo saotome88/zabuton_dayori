0fd08b2b54d0d2805d6db3d98bc1684dd1365634
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-23 15:23:06 +0900

    管理者投稿:複数人投稿/class EventComedianForm

--- app/models/event_comedian_form.rb
+++ app/models/event_comedian_form.rb
@@ -2,18 +2,20 @@ class EventComedianForm
   include ActiveModel::Model
   include ActiveRecord::AttributeAssignment
   extend ActiveHash::Associations::ActiveRecordExtensions
-  attr_accessor :theater_id, :daynight_id, :startday, :comedian_id
-
+  attr_accessor :theater_id, :daynight_id, :startday, :comedian_id, :comedian_ids
   with_options presence: true do
     validates :theater_id
     validates :daynight_id
     validates :startday
-    validates :comedian_id
   end
 
-  def save
+  def save(comedians)
     event = Event.create(theater_id: theater_id, daynight_id: daynight_id, startday: startday)
-    EventComedian.create(event_id: event.id, comedian_id: comedian_id) 
+    i = 0
+    comedians.each do |comedians|
+      EventComedian.create(event_id: event.id, comedian_id: comedian_ids[i])
+      i += 1
+    end
   end
 
 end
