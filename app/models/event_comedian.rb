716654bb848d47060483fd2446abd8dca1a5a37b
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-24 22:34:40 +0900

    カレンダー:アソシエーション記述修正/event_comedian.rb

--- app/models/event_comedian.rb
+++ app/models/event_comedian.rb
@@ -1,5 +1,5 @@
 class EventComedian < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :comedian
+  belongs_to_active_hash :comedian
   belongs_to :event
 end
