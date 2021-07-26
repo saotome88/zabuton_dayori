f9cb9d720be731452da5dac4ba6a45a886b67a16
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-26 13:10:29 +0900

    カレンダー/ActiveHashの芸人追加(仮)

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -5,11 +5,15 @@ class Comedian < ActiveHash::Base
 
   self.data = [
     { id: 0, name: '' },
-    { id: 1, name: '春風亭　あああ' },
-    { id: 2, name: '柳家　いいい' },
-    { id: 3, name: '橘　ううう' },
-    { id: 4, name: '桂　えええ' },
-    { id: 5, name: '三遊亭　おおお' },
-    { id: 6, name: '神田　かかか' }
+    { id: 1, name: '春風亭　太郎' },
+    { id: 2, name: '柳家　次郎' },
+    { id: 3, name: '橘　三郎' },
+    { id: 4, name: '桂　四郎' },
+    { id: 5, name: '三遊亭　五郎' },
+    { id: 6, name: '神田　六郎' },
+    { id: 7, name: '笑福亭　七郎' },
+    { id: 8, name: '柳亭　八郎' },
+    { id: 9, name: '五街道　九郎' },
+    { id: 10, name: '桃月　十郎' }
   ]
 end
