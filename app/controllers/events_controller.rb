5e4a440c4a4c0b9eb34623a2206065e8dbcf1eb5
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-26 13:08:32 +0900

    カレンダー:eventコントローラー/showアクション追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,11 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+    @events = Event.all
+    @show = Event.find(params[:id])
+    # 昼席と夜席の二つのデータを取得する
+    @shows = Event.where(start_time: @show.start_time, theater_id: @show.theater_id)
+  end
 end
