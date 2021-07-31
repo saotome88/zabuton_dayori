70fde80d9ec1fc7136c8dcd5af6e2b45ae130482
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-31 22:21:24 +0900

    index:webpaxkでbootstrap導入/application.jsに追加記述

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,6 @@ require("../calendar")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap';
+import '../stylesheets/application';
