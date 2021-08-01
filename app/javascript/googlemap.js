2bb6a85f71f2982ded8bf296233b48f70fc99f94
saotome88 <ryoji.saotome1988@gmail.com>
2021-08-01 20:20:57 +0900

    index/GoogleMapで各演芸場分の表示をさせた

--- app/javascript/googlemap.js
+++ app/javascript/googlemap.js
@@ -1,16 +1,39 @@
 function map() {
-  var MyLatLng = new google.maps.LatLng(35.69129219891356, 139.70593501342813);
-  var Options = {
-  zoom: 17,      //地図の縮尺値
-  center: MyLatLng,    //地図の中心座標
-  mapTypeId: 'roadmap'   //地図の種類
-  };
-  var map = new google.maps.Map(document.getElementById('map'), Options);
+  var latlng_01 = new google.maps.LatLng(35.69129219891356, 139.70593501342813);
+  var latlng_02 = new google.maps.LatLng(35.69129219891356, 139.70593501342813);
+  var latlng_03 = new google.maps.LatLng(35.713609437409076, 139.79316806904131);
+  var latlng_04 = new google.maps.LatLng(35.70937777898786, 139.7731515574054);
 
-  var marker = new google.maps.Marker({
-    position: MyLatLng,
-    map: map
-});
+  //地図のオプションを設定する
+  var myOptions_01 = {
+   zoom: 18,//拡大比率
+   center: latlng_01,//中心座標
+   mapTypeId: 'roadmap'//表示タイプ
+  };
+
+  var myOptions_02 = {
+   zoom: 18,//拡大比率
+   center: latlng_02,//中心座標
+   mapTypeId: 'roadmap'//表示タイプ
+  };
+
+  var myOptions_03 = {
+    zoom: 18,//拡大比率
+    center: latlng_03,//中心座標
+    mapTypeId: 'roadmap'//表示タイプ
+   };
+
+   var myOptions_04 = {
+    zoom: 18,//拡大比率
+    center: latlng_04,//中心座標
+    mapTypeId: 'roadmap'//表示タイプ
+   };
+
+  //canvasに地図のインスタンスを作成する
+  var map_01 = new google.maps.Map(document.getElementById('map_01'), myOptions_01);
+  var map_02 = new google.maps.Map(document.getElementById('map_02'), myOptions_02);
+  var map_03 = new google.maps.Map(document.getElementById('map_03'), myOptions_03);
+  var map_04 = new google.maps.Map(document.getElementById('map_04'), myOptions_04);
 };
 
 window.addEventListener('load', map);
