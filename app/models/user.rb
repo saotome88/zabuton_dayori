0c8a4537d46da906b037c7e5d78c146c8ad7096e
saotome88 <ryoji.saotome1988@gmail.com>
2021-08-02 12:18:23 +0900

    いいね機能:Userモデルでどの番組にいいねをしたか/すでにいいねをしたかを取得

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,13 @@ class User < ApplicationRecord
   # passwordは半角英数字のバリデーション
   validates :encrypted_password, :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
 
-  has_many :reaction_likes, dependent: :destroy
   has_many :events, through: :reaction_likes
+  has_many :reaction_likes, dependent: :destroy
+  # ユーザーがどの番組に「いいねをした」かを取得する
+  has_many :liked_events, through: :likes, source: :event
+
+  # ユーザーがすでにいいねをしているかどうかの判定をする。
+  def already_liked?(event)
+    self.likes.exists?(event_id: event.id)
+  end
 end
