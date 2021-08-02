074d15a4331c7925ea3b53bdc295e05fe874c0be
saotome88 <ryoji.saotome1988@gmail.com>
2021-08-02 12:24:43 +0900

    いいね機能:reaction_likesコントローラーでcreate/destroyアクションの設定

--- app/controllers/reaction_likes_controller.rb
+++ app/controllers/reaction_likes_controller.rb
@@ -1,11 +1,15 @@
 class ReactionLikesController < ApplicationController
   before_action :authenticate_user!
-  def new
-  end
-  
   def create
+    @reaction_like = current_user.reaction_likes.create(event_id: params[:event_id])
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    # findメソッドはidを引数に取る。find_byメソッドは属性を取り、複数条件を指定できる。
+    @reaction_like = ReactionLike.find_by(event_id: params[:event_id], user_id: current_user.id)
+    @reaction_like.destroy
+    #直前のぺージにリダイレクトする。いいねを消したら、トップページをリダイレクトして反映させている
+    redirect_back(fallback_location: root_path)
   end
 end
