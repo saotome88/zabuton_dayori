0129d48294c16ea0758096cfc779f7dfa9501916
saotome88 <ryoji.saotome1988@gmail.com>
2021-08-02 22:30:20 +0900

    migration.rbの修正/references型のカラムでnullを削除

--- db/migrate/20210716141634_create_event_comedians.rb
+++ db/migrate/20210716141634_create_event_comedians.rb
@@ -2,7 +2,7 @@ class CreateEventComedians < ActiveRecord::Migration[6.0]
   def change
     create_table :event_comedians do |t|
       t.timestamps
-      t.references :event,           null: false, foreign_key: true
+      t.references :event,           foreign_key: true
       t.integer    :comedian_id,     null: false
     end
   end
