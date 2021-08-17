9aaa9c13070a16ef6f68d70156cee1a9c2ec417f
saotome88 <ryoji.saotome1988@gmail.com>
2021-08-17 23:50:47 +0900

    一括登録:EventComedianForm/saveアクション追記

--- app/models/event_comedian_form.rb
+++ app/models/event_comedian_form.rb
@@ -2,19 +2,33 @@ class EventComedianForm
   include ActiveModel::Model
   include ActiveRecord::AttributeAssignment
   extend ActiveHash::Associations::ActiveRecordExtensions
-  attr_accessor :theater_id, :daynight_id, :start_time, :comedian_id, :comedian_ids
+#  attr_accessor :theater_id, :daynight_id, :start_time, :comedian_id, :comedian_ids
+  attr_accessor :theater_id, :daynight_id, :start_time, :start_times, :comedian_id, :comedian_ids
   with_options presence: true do
     validates :theater_id
     validates :daynight_id
-    validates :start_time
+#    validates :start_time
   end
 
-  def save(comedians)
-    event = Event.create(theater_id: theater_id, daynight_id: daynight_id, start_time: start_time)
-    i = 0
-    comedians.each do |comedians|
-      EventComedian.create(event_id: event.id, comedian_id: comedian_ids[i])
-      i += 1
+#  def save(comedians)
+#    event = Event.create(theater_id: theater_id, daynight_id: daynight_id, start_time: start_time)
+#    i = 0
+#    comedians.each do |comedians|
+#      EventComedian.create(event_id: event.id, comedian_id: comedian_ids[i])
+#      i += 1
+#    end
+#  end
+
+  def save(comedians, start_times)
+    x = 0
+    start_times.each do |start_time|
+      event = Event.create(theater_id: theater_id, daynight_id: daynight_id, start_time: start_times[x])
+      i = 0
+      comedians.each do |comedians|
+        EventComedian.create(event_id: event.id, comedian_id: comedian_ids[i])
+        i += 1
+      end
+      x += 1
     end
   end
 
