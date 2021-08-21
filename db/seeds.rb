85101843c5297a8f45ed7f4e937dc3751d89d6a9
saotome88 <ryoji.saotome1988@gmail.com>
2021-08-21 22:17:02 +0900

    seed.rbにてテスト用アカウントを作成する

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# 管理者用アカウント
 User.create!(nickname:  "管理者:早乙女",
   email: ENV["ADMIN_EMAIL"],
   password:  ENV["ADMIN_PASSWORD"],
   password_confirmation: ENV["ADMIN_PASSWORD"],
   admin: true)
+
+# テスト用ユーザーアカウント
+User.create!(nickname:  "テスト太郎",
+  email: "test@test.com",
+  password:  "test123",
+  password_confirmation: "test123",
+  admin: false)
